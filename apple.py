faa0491bd91012c7c620afcd97283111a8c717b5
coder-anish <anishkhanal27@gmail.com>
2021-02-23 10:06:14 +0545

    area of triangle

---
+++ apple.py
@@ -0,0 +1,3 @@
+# N students take K apples and distribute them among each other evenly.the remaining part remains
+#in the basket how many apple will each student get ?the program
+# reads the number N and K . it should print 2 answer from the abo

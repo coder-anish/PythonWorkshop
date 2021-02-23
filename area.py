b7a6ca37c9a0de9a8a825d87088837b39062dcd5
coder-anish <anishkhanal27@gmail.com>
2021-02-23 09:44:23 +0545

    hello world

---
+++ area.py
@@ -0,0 +1,5 @@
+#area of right angled triangle
+base_length=float(input("enter your base length: "))
+height=float(input("enter your height: "))
+area=0.5* (base_length*height)
+print(f"the area of right angled triangle is {area} ")
---
+++ sum.py
@@ -0,0 +1,7 @@
+num_one=float(input("enter your number: "))
+num_two=float(input("enter your number: "))
+num_three=float(input("enter your number: "))
+
+sum=num_one+num_two+num_three
+
+print("the sum of three number are",sum)
